227db87e6fe1913eefc5893de23360af23a653d0
MatheusW14 <matheuscostamendes@hotmail.com>
2025-02-11 21:32:55 -0300

    altera parâmetros de métodos na classe UserRepository e EmailService para usar 'new_user' em vez de 'user'

--- 01_srp.py
+++ 01_srp.py
@@ -9,27 +9,27 @@ class User:
 class UserRepository:
     """Classe responsável por salvar o usuário no banco de dados."""
 
-    def save(self, user: User):
+    def save(self, new_user: User):
         """
         Saves the given user to the database.
 
         Args:
-            user (User): The user object to be saved, containing user details.
+            new_user (User): The user object to be saved, containing user details.
         """
-        print(f"Saving user {user.name} to the database.")
+        print(f"Saving user {new_user.name} to the database.")
 
 
 class EmailService:
     """Classe responsável por enviar e-mails."""
 
-    def send_welcome_email(self, user: User):
+    def send_welcome_email(self, new_user: User):
         """
         Sends a welcome email to the specified user.
 
         Args:
-            user (User): The user to whom the welcome email will be sent.
+            new_user (User): The user to whom the welcome email will be sent.
         """
-        print(f"Sending welcome email to {user.email}.")
+        print(f"Sending welcome email to {new_user.email}.")
 
 
 # Exemplo de uso
